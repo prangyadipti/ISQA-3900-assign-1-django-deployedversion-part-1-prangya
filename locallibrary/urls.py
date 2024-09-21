444d837e9204ad8bc066961fc39913a5c481618d
prangyadipti <pchoudhury@unomaha.edu>
2024-09-21 05:56:21 -0500

    urls.py update
    The urls.py file for the main application
    added server and setting and re_path

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':
+
+                                                 settings.MEDIA_ROOT}),  # serve media files when deployed
+
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
+
+                                                  settings.STATIC_ROOT}),  # serve static files when deployed
+
 ]
